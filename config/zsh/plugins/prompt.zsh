a1d49aa95f111256f15104fd7e26b9c186ee7f80
Hexin <hexium310@gmail.com>
2018-10-28 05:50:04 +0900

    Fix space of PS1

--- config/zsh/plugins/prompt.zsh
+++ config/zsh/plugins/prompt.zsh
@@ -1,8 +1,8 @@
 autoload -Uz vcs_info
 autoload -Uz add-zsh-hook
 
-zstyle ':vcs_info:*' formats '(%b)' '%c%u'
-zstyle ':vcs_info:*' actionformats '(%b|%a)'
+zstyle ':vcs_info:*' formats '%F{blue}(%b)' '%c%u'
+zstyle ':vcs_info:*' actionformats '%F{blue}(%b|%a)'
 zstyle ':vcs_info:git:*' check-for-changes true
 zstyle ':vcs_info:git:*' stagedstr '%F{yellow}!'
 zstyle ':vcs_info:git:*' unstagedstr '%F{red}+'
@@ -13,10 +13,11 @@ precmd() {
 }
 
 _update_vcs_info_msg() {
-    local user_host='%B%F{green}%n@%m%f%b'
-    local directory='%B%F{blue}%~%f%b'
+    local user_host='%F{green}%n@%m%f'
+    local directory='%F{blue}%~%f'
     local vcs="$vcs_info_msg_1_$vcs_info_msg_0_"
-    export PS1=`echo "$user_host $directory %B%F{blue}$vcs%f%b %B%F{blue}$%f%b " | sed -E 's/[[:space:]]+/ /g'`
+    #echo "$user_host $directory $vcs %B%F{blue}$%f%b " | sed -E 's/[[:space:]]+/ /g'
+    export PS1=`echo "%B$user_host $directory $vcs %F{blue}$%f %b" | sed -E 's/[[:space:]]+/ /g'`
 }
 
 add-zsh-hook precmd _update_vcs_info_msg

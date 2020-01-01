18768a700c28f5ea611656a76507907fe10003b8
Hexin <hexium310@gmail.com>
2020-01-02 03:43:22 +0900

    Add fzf completions for yarn

--- config/zsh/plugins/fzf.zsh
+++ config/zsh/plugins/fzf.zsh
@@ -107,3 +107,77 @@ _fzf_complete_git_tabularize() {
         }
     '
 }
+
+_fzf_complete_yarn() {
+    if [[ "$@" = 'yarn'* ]] || [[ "$@" = 'yarn run'* ]]; then
+        _fzf_complete '--ansi' "$@" < <(
+            yarn run 2> /dev/null |
+            awk -F ',' '
+            NR == 2 {
+                gsub(/^.+: /, "")
+                gsub(/, /, "\n")
+                binaries = $0
+            }
+            /^ +-/ {
+                gsub(/^ +- /, "")
+                command = $0
+                getline
+                gsub(/^ +/, "")
+                projects[(NR - 1) / 2 - 1] = sprintf("%s,%s", command, $0)
+            }
+            END {
+                for (i = 1; i <= length(projects); ++i) {
+                    print projects[i]
+                }
+                print binaries
+            }
+            ' |
+            _fzf_complete_yarn_tabularize
+        )
+        return
+    fi
+
+    zle ${fzf_default_completion:-expand-or-complete}
+}
+
+_fzf_complete_yarn_post() {
+    awk '
+    {
+        if ($1 ~ /\x27/) {
+            for (i = 2; i <= NF; ++i) {
+                if ($i ~ /\x27/) {
+                    for (j = 1; j <= i; ++j) {
+                        str = str " " $j
+                    }
+                }
+            }
+        } else {
+            str = $1
+        }
+
+        sub("^ ", "", str)
+        print str
+    }
+    '
+}
+
+_fzf_complete_yarn_tabularize() {
+    awk \
+        -F ',' \
+        -v yellow="$(tput setaf 3)" \
+        -v reset="$(tput sgr0)" '
+        {
+            refnames[NR] = $1
+            if (length($1) > refname_max) {
+                refname_max = length($1)
+            }
+            $1 = ""
+            messages[NR] = $0
+        }
+        END {
+            for (i = 1; i <= length(refnames); ++i) {
+                printf "%s%-" refname_max "s%s %s\n", yellow, refnames[i] ~ / / ? "\x27" refnames[i] "\x27" : refnames[i], reset, messages[i]
+            }
+        }
+    '
+}

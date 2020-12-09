3a37f71ea3156636f10e20a2b63f68275ec7cbe1
Hexin <hexium310@gmail.com>
2020-12-10 01:12:37 +0900

    Change lazy loading of Vim plugins

--- config/nvim/autoload/plugin/fugitive.vim
+++ config/nvim/autoload/plugin/fugitive.vim
@@ -1,5 +1,5 @@
 function! plugin#fugitive#set_maps() abort
-  nnoremap <silent> <Space>gs :Gstatus<Cr>
+  nnoremap <silent> <Space>gs :Git<Cr>
   nnoremap <silent> <Space>gw :Gwrite<Cr>
   nnoremap <silent> <Space>gd :Gdiff<Cr>
 endfunction
--- config/nvim/plugins/nvim-toggleterm.lua
+++ config/nvim/plugins/nvim-toggleterm.lua
@@ -1,3 +1,7 @@
+-- Call manually `require'toggleterm'.__set_highlights()` because VimEnter has
+-- already triggered when `require'toggleterm.setup {}` that call it is called.
+require'toggleterm'.__set_highlights(-30)
+
 require'toggleterm'.setup {
   size = 15,
   open_mapping = [[<C-t>]],

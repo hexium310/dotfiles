4b66fef9302a4e60e5edcd4c50544c8fef0c63d7
Hexin <hexium310@gmail.com>
2020-12-30 20:16:00 +0900

    Shorten $PROMPT

--- config/zsh/plugins/prompt.zsh
+++ config/zsh/plugins/prompt.zsh
@@ -35,38 +35,42 @@ _zsh_prompt_redraw() {
 _zsh_prompt() {
     local suffix
     local vcs_info_msg
-    local user_host='%F{green}%n@%m%f'
-    local directory='%F{blue}%2~%f'
+    local host
+    local directory='%F{blue%}%1~%f'
+    if [[ $HOST != *.local ]]; then
+        host='%m '
+    fi
+    local colored_host="%F{green%}$host%f"
 
     [[ -n $vcs_info_msg_0_ ]] && vcs_info_msg+=$vcs_info_msg_0_
-    [[ -n $vcs_info_msg_1_ ]] && vcs_info_msg+="%F{blue}$vcs_info_msg_1_%f"
-    [[ -n $vcs_info_msg_2_ ]] && vcs_info_msg+="%F{red}$vcs_info_msg_2_%f"
+    [[ -n $vcs_info_msg_1_ ]] && vcs_info_msg+="%F{blue%}$vcs_info_msg_1_%f"
+    [[ -n $vcs_info_msg_2_ ]] && vcs_info_msg+="%F{red%}$vcs_info_msg_2_%f"
 
     case $1 in
         insert)
-            suffix='%K{blue}%F{#393939} $ %f%k'
+            suffix='%K{blue%}%F{#393939%} $ %f%k'
             ;;
         visual)
-            suffix='%K{magenta}%F{#393939} $ %f%k'
+            suffix='%K{magenta%}%F{#393939%} $ %f%k'
             ;;
         normal)
-            suffix='%K{green}%F{#393939} $ %f%k'
+            suffix='%K{green%}%F{#393939%} $ %f%k'
             ;;
         finish)
-            suffix='%K{#898989}%F{#393939} $ %f%k'
-            user_host='%F{#89b789}%n@%m%f'
-            directory='%F{#5b89b7}%2~%f'
+            suffix='%K{#898989%}%F{#393939%} $ %f%k'
+            colored_host="%F{#89b789%}$host%f"
+            directory='%F{#5b89b7%}%1~%f'
             vcs_info_msg=${vcs_info_msg//\{blue\}/\{#5b89b7\}}
             vcs_info_msg=${vcs_info_msg//\{red\}/\{#d96b6d\}}
             vcs_info_msg=${vcs_info_msg//\{green\}/\{#89b789\}}
             vcs_info_msg=${vcs_info_msg//\{yellow\}/\{#e5b75b\}}
             ;;
         *)
-            suffix='%K{blue}%F{#393939} $ %f%k'
+            suffix='%K{blue%}%F{#393939%} $ %f%k'
             ;;
     esac
 
-    PROMPT="%B$user_host $directory $vcs_info_msg $suffix %b"
+    PROMPT="%B$colored_host$directory $vcs_info_msg $suffix %b"
     zle reset-prompt
 }
 

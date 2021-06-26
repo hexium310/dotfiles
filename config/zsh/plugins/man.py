52353026a885bb226d52db5201ee9c6bb0afa1a7
Hexin <hexium310@gmail.com>
2021-06-27 02:31:54 +0900

    Improve execution time of man.py

--- config/zsh/plugins/man.py
+++ config/zsh/plugins/man.py
@@ -7,5 +7,5 @@ if not address:
     sys.exit(0)
 
 nvim = pynvim.attach("socket", path=address);
-name = list(filter(lambda item: item != '\n', sys.stdin))[0].split()[0].lower()
+name = next(filter(lambda item: item != '\n', sys.stdin)).split(maxsplit = 1)[0].lower()
 nvim.command(f'Man {name}')

52a6601201f7802ac2d8c8bbc48ca5283b118052
Hexin <hexium310@gmail.com>
2021-09-03 03:55:19 +0900

    Enable nvim-treesitter for some languages including lua and vim

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,30 +1,36 @@
 local function init()
-  require'nvim-treesitter.configs'.setup {
-    ensure_installed = {
-      -- "bash",
-      -- "css",
-      -- "javascript",
-      -- "json",
-      -- "query",
-      -- "toml",
-      "tsx",
-      "typescript",
-      -- "yaml",
-    },
-    indnet = {
-      enable = false,
-    },
-    highlight = {
-      enable = false,
-    },
+  local disables = {
+    'bash',
+    'css',
+    'javascript',
+    'query',
+    'tsx',
+    'typescript',
   }
 
-  vim.cmd([[
-    augroup nvim-treesitter
-      autocmd!
-      autocmd FileType vim :TSBufDisable highlight
-    augroup END
-  ]])
+  require'nvim-treesitter.configs'.setup {
+    ensure_installed = {
+      'bash',
+      'css',
+      'javascript',
+      'json',
+      'lua',
+      'query',
+      'toml',
+      'tsx',
+      'typescript',
+      'vim',
+      'yaml',
+    },
+    indent = {
+      disable = disables,
+      enable = true,
+    },
+    highlight = {
+      disable = disables,
+      enable = true,
+    },
+  }
 end
 
 return init

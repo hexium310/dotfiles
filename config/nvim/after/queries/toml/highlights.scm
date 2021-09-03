51ae9e787fb11db28c0827876104fc526618365c
Hexin <hexium310@gmail.com>
2021-09-04 03:55:18 +0900

    Add language injections in toml for dein.vim
    There is an issue where a highlight of `'''` comes to none.

---
+++ config/nvim/after/queries/toml/highlights.scm
@@ -0,0 +1,9 @@
+(
+  (table_array_element
+    (pair
+      (bare_key) @_hook_identifier
+      (string) @none
+    )
+  )
+  (#contains? @_hook_identifier "hook_")
+)
---
+++ config/nvim/queries/toml/injections.scm
@@ -0,0 +1,10 @@
+(
+  (table_array_element
+    (pair
+      (bare_key) @_hook_identifier
+      (string) @vim
+    )
+  )
+  (#contains? @_hook_identifier "hook_")
+  (#offset! @vim 0 3 0 -3)
+)

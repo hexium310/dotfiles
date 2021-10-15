743572f9e4d345c8943da65e4ed4453804fd7ca3
Hexin <hexium310@gmail.com>
2021-10-16 03:09:18 +0900

    Remove hack implemented in nvim-autopairs

--- config/nvim/lua/plugins/autopairs.lua
+++ config/nvim/lua/plugins/autopairs.lua
@@ -1,8 +1,3 @@
-local function replace_bs(key)
-  local utils = require('nvim-autopairs/utils')
-  return string.gsub(key, utils.esc('<Right><BS>'), utils.esc('<Del>'))
-end
-
 local function init()
   local autopairs = require('nvim-autopairs')
   local rule = require('nvim-autopairs/rule')
@@ -28,16 +23,6 @@ local function init()
         return vim.tbl_contains({ '()', '[]', '{}' }, pair)
       end),
   })
-
-  local original_autopairs_bs = autopairs.autopairs_bs
-  autopairs.autopairs_bs = function (bufnr)
-    return replace_bs(original_autopairs_bs(bufnr))
-  end
-
-  local original_autopairs_c_w = autopairs.autopairs_c_w
-  autopairs.autopairs_c_w = function (bufnr)
-    return replace_bs(original_autopairs_c_w(bufnr))
-  end
 end
 
 return init

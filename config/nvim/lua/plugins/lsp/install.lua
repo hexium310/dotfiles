59900746215c0a32e535dee495c2cae72091b4d0
Hexin <hexium310@gmail.com>
2021-10-17 01:00:50 +0900

    Support script to update LSP for nvim-lsp-installer

--- config/nvim/lua/plugins/lsp/install.lua
+++ config/nvim/lua/plugins/lsp/install.lua
@@ -2,14 +2,24 @@ local M = {}
 
 local lsp_installer_servers = require('nvim-lsp-installer.servers')
 
-function M.install(servers)
+function M.install(servers, force)
   for _, s in ipairs(servers) do
     local ok, server = lsp_installer_servers.get_server(s)
-    if not ok or server:is_installed() then
+    if not ok or (not force and server:is_installed()) then
       return
     end
 
     server:install()
+  end
+end
+
+function M.update(force)
+  local updates = require('plugins/lsp/update').check_update()
+
+  M.install(vim.tbl_keys(updates), force or false)
+
+  if vim.tbl_count(updates) > 0 then
+    print(vim.inspect(updates))
   end
 end
 
--- config/nvim/lua/plugins/lsp/update.lua
+++ config/nvim/lua/plugins/lsp/update.lua
@@ -1,7 +1,5 @@
 local M = {}
 
-local lsp_install_path = require('lspinstall/util').install_path
-
 --- @param cmd string | string[]
 --- @param opts table
 --- @return string @Raw data
@@ -20,16 +18,15 @@ local function job(cmd, opts)
   return result
 end
 
---- @param lang string
---- @param packages string[]
+--- @param server Server
 --- @return table @JSON
-local function npm(lang, packages)
-  return vim.fn.json_decode(job(vim.list_extend({
+local function npm(server)
+  return vim.fn.json_decode(job({
     'npm',
     'outdated',
     '--json',
-  }, packages), {
-    cwd = lsp_install_path(lang),
+  }, {
+    cwd = server.root_dir,
   }))
 end
 
@@ -61,24 +58,11 @@ local function build_npm_results(json)
 end
 
 local languages = {
-  json = function ()
-    local current_version = vim.fn.json_decode(vim.fn.readfile(lsp_install_path('json') .. '/vscode-json/json-language-features/server/package.json')).version
-    local tag_name = github_release('microsoft/vscode').tag_name
-    local latest_version = vim.fn.json_decode(job({
-      'curl',
-      '-s',
-      string.format('https://raw.githubusercontent.com/microsoft/vscode/%s/extensions/json-language-features/server/package.json', tag_name),
-    })).version
-
-    return {
-      ['json-language-features'] = {
-        current_version = current_version,
-        latest_version = latest_version,
-      },
-    }
+  json = function (server)
+    return build_npm_results(npm(server))
   end,
-  lua = function ()
-    local current_version = vim.fn.json_decode(vim.fn.readfile(lsp_install_path('lua') .. '/sumneko-lua/extension/package.json')).version
+  lua = function (server)
+    local current_version = vim.fn.json_decode(vim.fn.readfile(server.root_dir .. '/sumneko-lua/extension/package.json')).version
     local latest_version = string.match(github_release('sumneko/vscode-lua').tag_name, '%d+%.%d+%.%d+')
 
     return {
@@ -88,9 +72,9 @@ local languages = {
       },
     }
   end,
-  rust = function ()
+  rust = function (server)
     local current_version = vim.split(job({
-      lsp_install_path('rust') .. '/rust-analyzer',
+      server.root_dir .. '/rust-analyzer',
       '--version',
     }), ' ')[3]
     local latest_version = github_release('rust-analyzer/rust-analyzer').tag_name
@@ -102,38 +86,43 @@ local languages = {
       },
     }
   end,
-  tailwindcss = function ()
-    return build_npm_results(npm('tailwindcss', { '@tailwindcss/language-server' }))
+  tailwindcss = function (server)
+    return build_npm_results(npm(server))
   end,
-  typescript = function ()
-    return build_npm_results(npm('typescript', { 'typescript-language-server', 'typescript' }))
+  tsserver = function (server)
+    return build_npm_results(npm(server))
   end,
-  yaml = function ()
-    return build_npm_results(npm('yaml', { 'yaml-language-server' }))
+  yamlls = function (server)
+    return build_npm_results(npm(server))
   end,
-  vim = function ()
-    return build_npm_results(npm('vim', { 'vim-language-server' }))
+  vimls = function (server)
+    return build_npm_results(npm(server))
   end,
 }
 
 function M.check_update()
-  local installed_servers = require('lspinstall').installed_servers()
   local update_available_servers = {}
-  for _, server in ipairs(vim.tbl_filter(function (v) return type(languages[v]) == 'function' end, installed_servers)) do
-    local update_available_packages = {}
 
-    for package, value in pairs(languages[server]()) do
-      if value.current_version ~= value.latest_version then
-        update_available_packages = vim.tbl_extend('error', update_available_packages, {
-          [package] = value,
+  for _, server in ipairs(require('nvim-lsp-installer').get_installed_servers()) do
+    local name = server.name
+    local func = languages[name]
+
+    if type(func) == 'function' then
+      local update_available_packages = {}
+
+      for package, value in pairs(func(server)) do
+        if value.current_version ~= value.latest_version then
+          update_available_packages = vim.tbl_extend('error', update_available_packages, {
+            [package] = value,
+          })
+        end
+      end
+
+      if not vim.tbl_isempty(update_available_packages) then
+        update_available_servers = vim.tbl_extend('error', update_available_servers, {
+          [server.name] = update_available_packages,
         })
       end
-    end
-
-    if not vim.tbl_isempty(update_available_packages) then
-      update_available_servers = vim.tbl_extend('error', update_available_servers, {
-        [server] = update_available_packages,
-      })
     end
   end
 

eac6284acc5c2a87b79abcb7620f39ba70aeeaa7
Hexin <hexium310@gmail.com>
2021-11-16 01:56:59 +0900

    Fix issue where servers not installed are not installed newly
    In addition, print newly installed servers

--- config/nvim/lua/plugins/lsp/install.lua
+++ config/nvim/lua/plugins/lsp/install.lua
@@ -3,13 +3,16 @@ local M = {}
 local lsp_installer_servers = require('nvim-lsp-installer.servers')
 
 function M.install(servers, force)
-  for _, s in ipairs(servers) do
+  local uninstalled_servers = vim.tbl_filter(function(server)
+    return force or server and not server:is_installed()
+  end, vim.tbl_map(function (s)
     local ok, server = lsp_installer_servers.get_server(s)
-    if not ok or (not force and server:is_installed()) then
-      return
-    end
+    return ok and server
+  end, servers))
 
+  for _, server in ipairs(uninstalled_servers) do
     server:install()
+    print(server.name .. ' has been installed.')
   end
 end
 

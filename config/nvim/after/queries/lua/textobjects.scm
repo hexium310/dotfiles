05da7b0e145dec2cdb1a499e9f7c9745ed684ae2
Hexin <hexium310@gmail.com>
2022-01-21 02:45:08 +0900

    Adapt tree-sitter query to new Lua parser

--- config/nvim/after/queries/lua/textobjects.scm
+++ config/nvim/after/queries/lua/textobjects.scm
@@ -1,8 +1,8 @@
-(table
+(table_constructor
   "," @_start .
   (_) @parameter.inner
   (#make-range! "parameter.outer" @_start @parameter.inner))
-(table
+(table_constructor
   . (_) @parameter.inner
   . ","? @_end
   (#make-range! "parameter.outer" @parameter.inner @_end))

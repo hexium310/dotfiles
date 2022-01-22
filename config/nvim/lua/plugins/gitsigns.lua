3d46614209cd752d1b13960bfd2fd0cb05307908
Hexin <hexium310@gmail.com>
2022-01-23 01:34:09 +0900

    Use on_attach for gitsigns.setup instead of keymaps

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -1,5 +1,10 @@
+local utils = require('plugins/utils')
+
 local function init()
-  require('gitsigns').setup({
+  local gitsigns = require('gitsigns')
+  local actions = require('gitsigns.actions')
+
+  gitsigns.setup({
     signs = {
       changedelete = {
         numhl = 'GitSignsChangeDeleteNr',
@@ -7,23 +12,27 @@ local function init()
     },
     signcolumn = false,
     numhl = true,
-    keymaps = {
-      noremap = true,
-      ['n ]g'] = [[<Cmd>lua require('gitsigns.actions').next_hunk()<CR>]],
-      ['n [g'] = [[<Cmd>lua require('gitsigns.actions').prev_hunk()<CR>]],
-      ['n <Space>h'] = [[<Cmd>lua require('gitsigns.actions').preview_hunk()<CR>]],
-      ['n <Space>gr'] = [[<Cmd>lua require('gitsigns.actions').reset_hunk()<CR>]],
-      ['v <Space>gr'] = [[<Cmd>lua require('gitsigns').reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })<CR>]],
-      ['o ah'] = [[:<C-U>lua require('gitsigns.actions').select_hunk()<CR>]],
-      ['x ah'] = [[:<C-U>lua require('gitsigns.actions').select_hunk()<CR>]],
-      ['o ih'] = [[:<C-U>lua require('gitsigns.actions').select_hunk()<CR>]],
-      ['x ih'] = [[:<C-U>lua require('gitsigns.actions').select_hunk()<CR>]],
-    },
     preview_config = {
       border = 'double',
       -- Display a hunk preview over a LSP popup (zindex=50)
       zindex = 60,
-    }
+    },
+    on_attach = function (bufnr)
+      local maps = {
+        { 'n', ']g', actions.next_hunk },
+        { 'n', '[g', actions.prev_hunk },
+        { 'n', '<Space>h', actions.preview_hunk },
+        { 'n', '<Space>gr', actions.reset_hunk },
+        { 'v', '<Space>gr', function () gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end },
+        { { 'o', 'x' }, 'ah', actions.select_hunk },
+        { { 'o', 'x' }, 'ih', actions.select_hunk },
+      }
+
+      utils.set_keymaps(maps, {
+        buffer = bufnr,
+        silent = true,
+      })
+    end,
   })
 
   vim.cmd([[

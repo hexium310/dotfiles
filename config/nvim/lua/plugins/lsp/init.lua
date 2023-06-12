95a2e57070a76b01e2c2ac44bd5e7de072e63922
Hexin <hexium310@gmail.com>
2023-06-12 19:00:03 +0900

    Don't lazy-load mason

--- config/nvim/lua/plugins/lsp/init.lua
+++ config/nvim/lua/plugins/lsp/init.lua
@@ -1,14 +1,5 @@
 vim.lsp.stop_client(vim.lsp.get_active_clients())
 
-require('mason').setup({
-  ui = {
-    border = 'rounded',
-    keymaps = {
-      apply_language_filter = '<C-g>',
-    },
-  },
-})
-
 require('mason-lspconfig').setup({
   ensure_installed = {
     'eslint',
---
+++ config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,8 @@
+require('mason').setup({
+  ui = {
+    border = 'rounded',
+    keymaps = {
+      apply_language_filter = '<C-g>',
+    },
+  },
+})

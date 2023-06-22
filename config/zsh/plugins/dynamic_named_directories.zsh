0c812fa003535d013f6763c39cb7aa37eedec36a
Hexin <hexium310@gmail.com>
2023-06-23 02:57:38 +0900

    Use dynamic named directories in Zsh

---
+++ config/zsh/plugins/dynamic_named_directories.zsh
@@ -0,0 +1,37 @@
+_named_directoires() {
+    emulate -L zsh
+    setopt extended_glob
+
+    local -a directories
+    local expl
+    typeset -ga reply
+
+    if [[ $1 == 'n' ]]; then
+        local -a match mbegin mend
+        [[ $2 != r:(#b)(?*) ]] && return 1
+
+        directories=($HOME/Repositories/*/${match[1]}(N/))
+
+        (( ${#directories} != 1 )) && return 1
+
+        reply=(${directories[1]})
+        return 0
+    elif [[ $1 == 'd' ]]; then
+        directories=($HOME/Repositories/*/${2:t}(N/))
+
+        (( ${#directories} != 1 )) && return 1
+
+        reply=(${2:t} ${#2})
+        return 0
+    elif [[ $1 == 'c' ]]; then
+        directories=($HOME/Repositories/*/*(N/:t))
+        directories=(r:${^directories})
+        _tags named-directories
+        _tags && _requested named-directories expl 'dynamic named directories' &&
+            compadd $expl -S\] -- $directories
+        return 1
+    fi
+}
+
+autoload -Uz add-zsh-hook
+add-zsh-hook zsh_directory_name _named_directoires

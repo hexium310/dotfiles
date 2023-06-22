bb55d3aa7699de3c403b8372fdabd25514bc7820
Hexin <hexium310@gmail.com>
2023-06-23 04:12:43 +0900

    Don't overwrite function-local variable

--- config/nvim/lua/plugins/utils.lua
+++ config/nvim/lua/plugins/utils.lua
@@ -10,9 +10,8 @@ function M.set_keymaps(mappings, opts)
     local modes = v[1]
     local lhs = v[2]
     local rhs = v[3]
-    opts = vim.tbl_extend('keep', v[4] or {}, opts)
 
-    vim.keymap.set(modes, lhs, rhs, opts)
+    vim.keymap.set(modes, lhs, rhs, vim.tbl_extend('keep', v[4] or {}, opts))
   end
 end
 

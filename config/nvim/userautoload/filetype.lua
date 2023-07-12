1451f43c4ac636d0c92f44a47e21b1794b14a89b
Hexin <hexium310@gmail.com>
2023-07-13 01:58:23 +0900

    Set bufhidden=delete in gitcommit and gitrebase

--- config/nvim/userautoload/filetype.lua
+++ config/nvim/userautoload/filetype.lua
@@ -3,3 +3,11 @@ vim.filetype.add({
     zunit = 'zsh',
   },
 })
+
+local augroup = vim.api.nvim_create_augroup('init_filetype', {})
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'gitcommit', 'gitrebase' },
+  callback = function ()
+    vim.bo.bufhidden = 'delete'
+  end
+})

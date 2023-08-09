66106610463a72e9a4454af739957ecd3a45aea4
Hexin <hexium310@gmail.com>
2023-08-09 22:46:28 +0900

    Set backspace=start in DressingInput filetype only

--- config/nvim/userautoload/filetype.lua
+++ config/nvim/userautoload/filetype.lua
@@ -6,8 +6,26 @@ vim.filetype.add({
 
 local augroup = vim.api.nvim_create_augroup('init_filetype', {})
 vim.api.nvim_create_autocmd('FileType', {
+  group = augroup,
   pattern = { 'gitcommit', 'gitrebase' },
   callback = function ()
     vim.bo.bufhidden = 'delete'
+  end
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup,
+  pattern = { 'DressingInput' },
+  callback = function ()
+    local backspace = vim.opt.backspace
+    vim.opt.backspace:append({ 'start' })
+
+    vim.api.nvim_create_autocmd('WinLeave', {
+      group = augroup,
+      once = true,
+      callback = function ()
+        vim.opt.backspace = backspace
+      end
+    })
   end
 })

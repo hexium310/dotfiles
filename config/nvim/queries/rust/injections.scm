afce287f60a53fd48bd5d6f008b8fb56c771a45e
Hexin <hexium310@gmail.com>
2023-08-09 22:48:29 +0900

    Add regex injection to Rust on treesitter

---
+++ config/nvim/queries/rust/injections.scm
@@ -0,0 +1,22 @@
+;exntends
+(macro_invocation
+  (
+    (identifier) @_regex_macro
+    (#match? @_regex_macro "^(lazy_)?regex|regex_(captures|find|is_match|replace(_all)?)$")
+  )
+  (token_tree
+    (raw_string_literal) @regex
+  )
+)
+
+(macro_invocation
+  (scoped_identifier
+    path: (identifier) @_regex_path
+    (#match? @_regex_path "^lazy_regex$")
+    name: (identifier) @_regex_macro
+    (#match? @_regex_macro "^(lazy_)?regex|regex_(captures|find|is_match|replace(_all)?)$")
+  )
+  (token_tree
+    (raw_string_literal) @regex
+  )
+)

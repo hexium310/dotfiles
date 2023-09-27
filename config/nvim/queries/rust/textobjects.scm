2ed374fcb93c7eac672fadae7641d68213c6e487
Hexin <hexium310@gmail.com>
2023-09-28 04:47:12 +0900

    Add textobject for treating type parameter, type in tuple, and item in array as `@parameter`

--- config/nvim/queries/rust/textobjects.scm
+++ config/nvim/queries/rust/textobjects.scm
@@ -1,5 +1,26 @@
 ;extends
 
+((parameters
+  "," @_start . (type_identifier) @parameter.inner)
+ (#make-range! "parameter.outer" @_start @parameter.inner))
+((parameters
+  . (type_identifier) @parameter.inner . ","? @_end)
+ (#make-range! "parameter.outer" @parameter.inner @_end))
+
+((tuple_type
+  "," @_start . (_) @parameter.inner)
+ (#make-range! "parameter.outer" @_start @parameter.inner))
+((tuple_type
+  . (_) @parameter.inner . ","? @_end)
+ (#make-range! "parameter.outer" @parameter.inner @_end))
+
+((array_expression
+  "," @_start . (_) @parameter.inner)
+ (#make-range! "parameter.outer" @_start @parameter.inner))
+((array_expression
+  . (_) @parameter.inner . ","? @_end)
+ (#make-range! "parameter.outer" @parameter.inner @_end))
+
 ((call_expression
   function: (field_expression
     value: (_) "." @_start

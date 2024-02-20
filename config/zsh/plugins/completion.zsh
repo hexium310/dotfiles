ba28413c5316ceb527d2462ca25358888cf0c52d
Hexin <hexium310@gmail.com>
2024-02-21 04:39:19 +0900

    Change list color `other-writable` in Zsh

--- config/zsh/plugins/completion.zsh
+++ config/zsh/plugins/completion.zsh
@@ -4,7 +4,7 @@ zstyle ':completion:*:default' menu select=1
 zstyle ':completion:*:sudo:*' command-path $PATH
 zstyle ':completion:*' ignore-@arents parent pwd ..
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-zstyle ':completion:*' list-colors 'di=34' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'cd=43;34'
+zstyle ':completion:*' list-colors 'di=34' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'cd=43;34' 'ow=30;42'
 
 ZSH_COMPLETION_DIR=$XDG_CACHE_HOME/zsh/completions
 if ! [[ -d $ZSH_COMPLETION_DIR ]]; then

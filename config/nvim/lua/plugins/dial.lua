7d694649e6f496072a17ed85f757408a4131c433
Hexin <hexium310@gmail.com>
2024-04-29 23:31:10 +0900

    Add dial.nvim instead of increment_activator

---
+++ config/nvim/lua/plugins/dial.lua
@@ -0,0 +1,10 @@
+local augend = require("dial.augend")
+require("dial.config").augends:register_group({
+  default = {
+    augend.integer.alias.decimal_int,
+    augend.integer.alias.hex,
+    augend.constant.alias.bool,
+    augend.semver.alias.semver,
+    augend.misc.alias.markdown_header,
+  },
+})

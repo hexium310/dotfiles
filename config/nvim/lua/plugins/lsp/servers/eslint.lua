89286da29973a6caf40431ecd2f36866ab434a8a
Hexin <hexium310@gmail.com>
2024-06-02 00:12:29 +0900

    Replace goto_next/prev with jump

--- config/nvim/lua/plugins/lsp/servers/eslint.lua
+++ config/nvim/lua/plugins/lsp/servers/eslint.lua
@@ -7,13 +7,15 @@ local M = {}
 local opts = {
   on_attach = function (client, bufnr)
     local namespace = vim.lsp.diagnostic.get_namespace(client.id)
-    local goto_opts = {
-      namespace = namespace,
+    local jump_opts = {
+      diagnostic = {
+        namespace = namespace,
+      },
       float = lsp_utils.float_opts,
     }
     local maps = {
-      { 'n', ']a', function () vim.diagnostic.goto_next(goto_opts) end },
-      { 'n', '[a', function () vim.diagnostic.goto_prev(goto_opts) end },
+      { 'n', ']a', function () vim.diagnostic.jump(vim.tbl_deep_extend('force', jump_opts, { count = 1 })) end },
+      { 'n', '[a', function () vim.diagnostic.jump(vim.tbl_deep_extend('force', jump_opts, { count = -1 })) end },
       { 'n', '<F8>', function () vim.lsp.buf.format({
         async = true,
         filter = function (c)

74d245087eec5d2f06c64126eb7c01b446dd7d57
Hexin <hexium310@gmail.com>
2024-06-03 07:18:25 +0900

    Update enabled function parameter of lazydev

--- config/nvim/lua/plugins/lazydev.lua
+++ config/nvim/lua/plugins/lazydev.lua
@@ -2,10 +2,10 @@ require('lazydev').setup({
   library = {
     'luvit-meta/library',
   },
-  enabled = function(client)
+  enabled = function(root_dir)
     if vim.g.lazydev_enabled ~= nil then
       return vim.g.lazydev_enabled
     end
-    return client.root_dir and vim.fs.find('lua', { limit = math.huge, type = 'directory', path = client.root_dir }) and true or false
+    return vim.fs.find('lua', { limit = math.huge, type = 'directory', path = root_dir }) and true or false
   end,
 })

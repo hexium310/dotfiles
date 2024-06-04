969591541bc0e9084f126112db2b532be21eed9f
Hexin <hexium310@gmail.com>
2024-06-04 18:16:43 +0900

    Remove enabled of lazydev
    The default enabled() retruns ture

--- config/nvim/lua/plugins/lazydev.lua
+++ config/nvim/lua/plugins/lazydev.lua
@@ -2,10 +2,4 @@ require('lazydev').setup({
   library = {
     'luvit-meta/library',
   },
-  enabled = function(root_dir)
-    if vim.g.lazydev_enabled ~= nil then
-      return vim.g.lazydev_enabled
-    end
-    return vim.fs.find('lua', { limit = math.huge, type = 'directory', path = root_dir }) and true or false
-  end,
 })

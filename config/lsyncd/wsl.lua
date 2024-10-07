47092edc9840dd09a21f8b669c94d0c9918d1863
Hexin <hexium310@gmail.com>
2024-10-08 08:31:36 +0900

    Rename variable more appropriately

--- config/lsyncd/wsl.lua
+++ config/lsyncd/wsl.lua
@@ -1,18 +1,18 @@
 local xdg_config_home = os.getenv('XDG_CONFIG_HOME') or '/home/hexin/.config'
 local appdata = os.getenv('APPDATA') or '/mnt/c/Users/Hexin/AppData/Roaming'
 
-local transitions = {
+local transfers = {
   {
     source = ('%s/alacritty/'):format(xdg_config_home),
     target = ('%s/alacritty/'):format(appdata),
   },
 }
 
-for _, transition in ipairs(transitions) do
+for _, transfer in ipairs(transfers) do
   sync({
     default.rsync,
     delay = 0,
-    source = transition.source,
-    target = transition.target,
+    source = transfer.source,
+    target = transfer.target,
   })
 end

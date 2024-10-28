9c5898ec9565190a32d7f954d25387ff83f403d0
Hexin <hexium310@gmail.com>
2024-10-29 07:43:00 +0900

    Add textobjects for parameter in array or object of ECMAScript

--- config/nvim/queries/ecma/textobjects.scm
+++ config/nvim/queries/ecma/textobjects.scm
@@ -13,3 +13,41 @@
     property: (property_identifier) @method.inner)
   arguments: (arguments) @_end
   (#make-range! "method.outer" @_start @_end))
+
+; @parameter.inner
+; [firstItem, secondItem, thirdItem]
+;  |<----->|  |<------>|  |<----->|
+; @parameter.outer
+; [firstItem, secondItem, thirdItem]
+;  |<------>|
+;           |<-------->||<------->|
+((array
+  "," @_start
+  .
+  (_) @parameter.inner)
+ (#make-range! "parameter.outer" @_start @parameter.inner))
+((array
+   .
+  (_) @parameter.inner
+  .
+  ","? @_end)
+ (#make-range! "parameter.outer" @parameter.inner @_end))
+
+; @parameter.inner
+; { foo: value, bar: value, baz: value }
+;   |<------>|  |<------>|  |<------>|
+; @parameter.outer
+; { foo: value, bar: value, baz: value }
+;   |<------->|
+;             |<-------->||<-------->|
+(object
+  "," @_start
+  .
+  (pair) @parameter.inner
+ (#make-range! "parameter.outer" @_start @parameter.inner))
+(object
+  .
+  (pair) @parameter.inner
+  .
+  ","? @_end
+ (#make-range! "parameter.outer" @parameter.inner @_end))

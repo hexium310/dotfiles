e833fa7f13d1148801f333dfa070ffa50306f380
Hexin <hexium310@gmail.com>
2024-11-20 03:09:24 +0900

    Replace if-elif-fi with case

--- config/zsh/plugins/dynamic_named_directories.zsh
+++ config/zsh/plugins/dynamic_named_directories.zsh
@@ -6,31 +6,35 @@ _named_directoires() {
     local expl
     typeset -ga reply
 
-    if [[ $1 == 'n' ]]; then
-        local -a match mbegin mend
-        [[ $2 != r:(#b)(?*) ]] && return 1
+    case $1 in
+        n)
+            local -a match mbegin mend
+            [[ $2 != r:(#b)(?*) ]] && return 1
 
-        directories=($HOME/Repositories/*/${match[1]}(N/))
+            directories=($HOME/Repositories/*/${match[1]}(N/))
 
-        (( ${#directories} != 1 )) && return 1
+            (( ${#directories} != 1 )) && return 1
 
-        reply=(${directories[1]})
-        return 0
-    elif [[ $1 == 'd' ]]; then
-        directories=($HOME/Repositories/*/${2:t}(N/))
+            reply=(${directories[1]})
+            return 0
+            ;;
+        d)
+            directories=($HOME/Repositories/*/${2:t}(N/))
 
-        (( ${#directories} != 1 )) || [[ "${directories[0]}" != "$PWD" ]] && return 1
+            (( ${#directories} != 1 )) || [[ "$directories" != "$PWD" ]] && return 1
 
-        reply=(${2:t} ${#2})
-        return 0
-    elif [[ $1 == 'c' ]]; then
-        directories=($HOME/Repositories/*/*(N/:t))
-        directories=(r:${^directories})
-        _tags named-directories
-        _tags && _requested named-directories expl 'dynamic named directories' &&
-            compadd $expl -S\] -- $directories
-        return 1
-    fi
+            reply=(${2:t} ${#2})
+            return 0
+            ;;
+        c)
+            directories=($HOME/Repositories/*/*(N/:t))
+            directories=(r:${^directories})
+            _tags named-directories
+            _tags && _requested named-directories expl 'dynamic named directories' &&
+                compadd $expl -S\] -- $directories
+            return 1
+            ;;
+    esac
 }
 
 autoload -Uz add-zsh-hook

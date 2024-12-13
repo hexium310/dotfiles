32b64839fb02bbae8627a8a9608e26fadb1067b3
Hexin <hexium310@gmail.com>
2024-12-13 18:35:20 +0900

    Use gclip (golang-design/clipboard) to paste from clipboard in WSL

--- config/nvim/userautoload/clipboard.lua
+++ config/nvim/userautoload/clipboard.lua
@@ -1,4 +1,42 @@
 -- The builtin clipboard detection is slow because executable() is called many times.
+
+local function gclip_path()
+  local linux = vim.fs.joinpath(vim.env.GOPATH, 'bin/gclip')
+  local windows = vim.fs.joinpath(vim.env.USERPROFILE, 'go/bin/gclip')
+
+  if vim.fn.executable(linux) == 1 then
+    return linux
+  end
+
+  if vim.fn.executable(windows) == 1 then
+    return windows
+  end
+
+  return nil
+end
+
+if vim.fn.has('wsl') then
+  local gclip = gclip_path()
+  -- gclip -copy blocks process
+  local copy = { '/mnt/c/Windows/System32/clip.exe' }
+  local paste = gclip ~= nil and { gclip, '-paste' } or function () return '' end
+
+  vim.g.clipboard = {
+    name = 'wsl',
+    copy = {
+      ['+'] = copy,
+      ['*'] = copy,
+    },
+    paste = {
+      ['+'] = paste,
+      ['*'] = paste,
+    },
+    chache_enabled = 0,
+  }
+
+  return
+end
+
 if vim.fn.empty(vim.env.TMUX) ~= 1 then
   vim.g.clipboard = {
     name = 'tmux',
@@ -9,24 +47,6 @@ if vim.fn.empty(vim.env.TMUX) ~= 1 then
     paste = {
       ['+'] = { 'tmux', 'save-buffer', '-' },
       ['*'] = { 'tmux', 'save-buffer', '-' },
-    },
-    chache_enabled = 0,
-  }
-
-  return
-end
-
-if vim.fn.has('wsl') then
-  vim.g.clipboard = {
-    name = 'wsl',
-    copy = {
-      ['+'] = { 'clip.exe' },
-      ['*'] = { 'clip.exe' },
-    },
-    -- Ignores getting the clipboard content because the way of using powershell.exe or win32yank.exe is slow.
-    paste = {
-      ['+'] = function () return { "" } end,
-      ['*'] = function () return { "" } end,
     },
     chache_enabled = 0,
   }

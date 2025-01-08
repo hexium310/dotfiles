0a1fed104bfcf66ba7c29cfee4b9c71a040d6f55
Hexin <hexium310@gmail.com>
2025-01-08 18:42:46 +0900

    Stop reading .git/HEAD frequently
    FugitiveHead() reads .git/HEAD every time, so using it in lualine
    accesses that file frequently.

--- config/nvim/lua/plugins/lualine/extensions/fugitive.lua
+++ config/nvim/lua/plugins/lualine/extensions/fugitive.lua
@@ -1,6 +1,11 @@
 local onedark = require('onedark.colors')
 local theme = require('lualine.themes.onedark')
 
+local function git_branch()
+  -- gets a branch in `Heads: <branch>` in the first line of the fugitive buffer
+  return vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]:sub(7)
+end
+
 local function git_root()
   return vim.fn.fnamemodify(vim.fn.FugitiveGitDir(), ':~:h')
 end
@@ -14,7 +19,7 @@ local fugitive = {
     },
     lualine_b = {
       {
-        'FugitiveHead',
+        git_branch,
         color = function ()
           return {
             fg = onedark.black.gui,
@@ -29,7 +34,7 @@ local fugitive = {
   },
   inactive_sections = {
     lualine_b = {
-      'FugitiveHead',
+      git_branch,
       git_root,
     },
     lualine_y = {

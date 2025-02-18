4b7dc9a8a427b9b5dac7c23cc35d94c49e6ba0d6
Hexin <hexium310@gmail.com>
2025-02-19 01:53:23 +0900

    Stop redundant execution for printing docker contaienrs in tmux

--- config/tmux/plugins/docker_containers/docker_containers.sh
+++ config/tmux/plugins/docker_containers/docker_containers.sh
@@ -1,32 +1,32 @@
 docker_containers() {
-  docker ps --quiet |
-    xargs docker inspect --format '{
-      "name": {{json .Name}} {{with (index .Config.Labels "com.docker.compose.project")}},
-      "com.docker.compose.project": {{json .}} {{end}}
-    }' |
+  docker inspect --format '{
+    "name": {{json .Name}} {{with (index .Config.Labels "com.docker.compose.project")}},
+    "com.docker.compose.project": {{json .}} {{end}}
+  }' $(docker ps --quiet) |
     jq --slurp --from-file "$XDG_CONFIG_HOME/tmux/plugins/docker_containers/docker_containers.jq"
 }
 
 compose_containers() {
-  jq --raw-output '.composes | .[] | .project + "(" + (.count | tostring) + ")"' <<< $(docker_containers)
+  jq --raw-output '.composes | .[] | .project + "(" + (.count | tostring) + ")"'
 }
 
 containers() {
-  jq --raw-output '.containers[]' <<< $(docker_containers)
+  jq --raw-output '.containers[]'
 }
 
 main() {
   local shows=""
+  local list=$(docker_containers)
 
   while [[ $# -gt 0 ]]; do
     case $1 in
       --compose)
-        shows+=$(compose_containers)
+        shows+=$(compose_containers <<< $list)
         shows+=$'\n'
         shift
         ;;
       --container)
-        shows+=$(containers)
+        shows+=$(containers <<< $list)
         shows+=$'\n'
         shift
         ;;

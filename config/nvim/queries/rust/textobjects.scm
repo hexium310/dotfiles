8998d152419109dcbc6b2cc51b51ad36134f3601
Hexin <hexium310@gmail.com>
2025-10-16 04:21:00 +0900

    Add textobjects query for field identifier in rust

--- config/nvim/queries/rust/textobjects.scm
+++ config/nvim/queries/rust/textobjects.scm
@@ -1,24 +1,38 @@
 ;extends
 
-((array_expression
+(array_expression
   "," @parameter.outer
   .
-  _ @parameter.inner @parameter.outer))
-((array_expression
+  _ @parameter.inner @parameter.outer)
+(array_expression
   .
   _ @parameter.inner @parameter.outer
   .
-  ","? @parameter.outer))
+  ","? @parameter.outer)
 
+; Method
 ; @method.inner
-; foo.methodA(args).methodB()
-;     |<--->|       |<--->|
+; foo.identifier.methodA(args).methodB()
+;                |<--->|       |<--->|
 ; @method.outer
-; foo.methodA(args).methodB()
-;    |<---------->||<------>|
-((call_expression
+; foo.identifier.methodA(args).methodB()
+;               |<---------->||------|
+(call_expression
   function: (field_expression
+              value: _
+              "." @method.outer
+              field: (field_identifier) @method.inner @method.outer)
+  arguments: (arguments) @method.outer)
+
+; Field identifier
+; @method.inner
+; foo.identifier.methodA(args).methodB()
+;     |<------>|
+; @method.outer
+; foo.identifier.methodA(args).methodB()
+;    |<------->|
+(field_expression
+  (field_expression
     value: _
     "." @method.outer
-    field: (field_identifier) @method.inner @method.outer)
-  arguments: (arguments) @method.outer))
+    field: (field_identifier) @method.inner @method.outer))
